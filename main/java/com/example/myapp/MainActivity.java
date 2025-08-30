bfdd225d245abc9802b399326804a3cf450a89d5
jonlila802-hash <jonlila802@gmail.com>
2025-08-30 14:21:45 +0330

    Create MainActivity.java

---
+++ main/java/com/example/myapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "برنامه با موفقیت اجرا شد!", Toast.LENGTH_SHORT).show();
+    }
+}
